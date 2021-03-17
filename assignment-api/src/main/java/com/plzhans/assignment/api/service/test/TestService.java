5a05d2ed5574f102312d3b5e81eff630a4301aa0
plzhans <plzhans@gmail.com>
2021-03-17 12:06:35 +0900

    test 모듈 삭제

--- assignment-api/src/main/java/com/plzhans/assignment/api/service/test/TestService.java
+++
@@ -1,9 +0,0 @@
-package com.plzhans.assignment.api.service.test;
-
-import com.plzhans.assignment.api.service.test.datatype.TestDto;
-
-import java.util.List;
-
-public interface TestService {
-    List<TestDto> getAllTests();
-}
